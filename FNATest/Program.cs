0e8097cc429aed58269fe5172e2d19200f7d4485
DESKTOP-NM8KA7G\Rob Jellinghaus <rjellinghaus@gmail.com>
2016-09-10 23:05:05 -0700

    Woohoo!  x64 version of FNATest works!!!

--- FNATest/Program.cs
+++ FNATest/Program.cs
@@ -13,6 +13,10 @@ namespace FNATest
         [STAThread]
         static void Main()
         {
+            // Force FNA to not load audio.
+            // set FNA_AUDIO_DISABLE_SOUND=1
+            Environment.SetEnvironmentVariable("FNA_AUDIO_DISABLE_SOUND", "1");
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
